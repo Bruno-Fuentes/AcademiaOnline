34884605b790d2e1a0ad7a0b4daed291d4087501
Bruno Fuentes <bruno.fuentes@alunos.ifsuldeminas.edu.br>
2024-08-27 22:20:43 -0300

    Delete AcademiaOnline directory

--- AcademiaOnline/app/static/script.js
+++
@@ -1,8 +0,0 @@
-// Script básico para interação futura
-document.addEventListener('DOMContentLoaded', function() {
-    const btnTreinar = document.querySelector('.btn-treinar');
-
-    btnTreinar.addEventListener('click', function() {
-        alert('Hora de Treinar!');
-    });
-});
