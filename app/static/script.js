8aaed26e62daf97a32532f7fb9a0c298830a3b56
Bruno Fuentes <bruno.fuentes@alunos.ifsuldeminas.edu.br>
2024-08-27 22:21:20 -0300

    Add files via upload

---
+++ app/static/script.js
@@ -0,0 +1,8 @@
+// Script básico para interação futura
+document.addEventListener('DOMContentLoaded', function() {
+    const btnTreinar = document.querySelector('.btn-treinar');
+
+    btnTreinar.addEventListener('click', function() {
+        alert('Hora de Treinar!');
+    });
+});
